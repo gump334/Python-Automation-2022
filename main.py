0949fb1ea6d0b74133e676f8c37768fca5668f7d
Terrell <gumpfinest3341983@gmail.com>
2022-03-04 12:13:02 +0000

    change code to use panndas library

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import yagmail
 import os
-import time
-import schedule
+#import time
+#import schedule
+import pandas
 
 
 def send_email():
-  sender = ""
-  receiver = ""
+  sender = "blackcloudgeeks@gmail.com"
+
 
   subject = "This is a python test"
 
   contents = """
   Hello, This is my first python code for this week.
   """
+  df = pandas.read_csv('contacts.csv')
+  print(df)
+  # for 
+  # yag = yagmail.SMTP(user=sender, password=os.getenv("WINPASSWORD"))
+  # yag.send(to=receiver,subject=subject, contents=contents)
+  # print("Email Sent!!!")
 
-  yag = yagmail.SMTP(user=sender, password=os.getenv("WINPASSWORD"))
-  yag.send(to=receiver,subject=subject, contents=contents)
-  print("Email Sent!!!")
 
-
-schedule.every(1).minutes.do(send_email)
-while True:
-  schedule.run_pending()
-  #time.sleep(1)
+# schedule.every(1).minutes.do(send_email)
+# while True:
+#   schedule.run_pending()
+#   #time.sleep(1)
   
 
 
